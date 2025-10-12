7dfc5f241b7e36717e869913ab4eef755cedd566
Daniel Belintani <danielbelintani@Daniels-MBP.work.home.arpa>
2025-10-12 16:14:18 -0300

    max-length fix

--- test-project/i18n-llm.config.js
+++ test-project/i18n-llm.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   sourceLanguage: 'en-US',
-  schemaPaths: ['./i18n.schema.json'],
+  //schemaPaths: ['./i18n.schema.json','./vegeta.schema.json'],
+  schemaPaths: ['./vegeta.schema.json'],
   outputDir: './locales',
   providerConfig: {
     model: 'gpt-4.1-mini',
