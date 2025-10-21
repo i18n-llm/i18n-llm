368fd37cec80636742dcf7f19b194bccabe03028
Daniel Belintani <daniel@belintani.com>
2025-10-21 00:58:27 -0300

    renamed obsolete schema sample.

--- test-project/i18n-llm.config.js
+++ test-project/i18n-llm.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   sourceLanguage: 'en-US',
-  schemaPaths: ['./i18n.schema.json','./vegeta.schema.json'],
+  schemaPaths: ['./pirate.schema.json', './vegeta.schema.json'],
   outputDir: './locales',
   providerConfig: {
     //model: 'gpt-4.1-mini',
-      provider: 'gemini',  // ← Mude aqui
-      model: 'gemini-2.5-flash',
-      apiKey: process.env.GEMINI_API_KEY
+    provider: 'gemini',
+    model: 'gemini-2.5-flash',
+    apiKey: process.env.GEMINI_API_KEY
   },
 };
